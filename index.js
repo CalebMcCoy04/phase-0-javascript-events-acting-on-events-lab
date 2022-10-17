e15591b06df6a40b9bd07b2b642651bf7f384394
Caleb McCoy <caleb_mccoy115@yahoo.com>
2022-10-17 10:48:48 -0500

    commit tree e742d7d2341b11f1162b9a54fad9ea38e05bebd2

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+const dodger = document.querySelector('#dodger');
+
+dodger.style.backgroundColor = "#ff69b4";
+
+dodger.style.left = "100px";
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    }
+});
+
+function moveDodgerLeft() {
+    const leftNumber = dodger.style.left.replace('px','');
+    const left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+};
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerRight() {
+    const rightNumber = dodger.style.left.replace('px','');
+    const right = parseInt(rightNumber, 10);
+
+    if (right < 360){
+        dodger.style.left = `${right + 1}px`;
+    }
+}
